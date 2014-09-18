a51ddb5f90afa2d5420a003f6dad072616b85776
System Administrator <root@Ricardos-MacBook-Pro-2.local>
2014-09-18 13:43:58 -0500

    3 folders

---
+++ 3 - Algorithms/hours_worked.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Algorithms</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/test.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    
+
+  </head>
+  <body>
+    
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <h1 class="text-center">Hours worked</h1>
+    </div>
+
+    <div class="container">
+
+      <form class="col-sm-6 col-md-offset-3" role="form">
+        <h2>Calculate the payment of an employee</h2>
+
+        <input id="hours" type="number" class="form-control" placeholder="Number of hours worked" required>
+
+        <input id="rate" type="number" class="form-control" placeholder="Hourly rate" required>
+       
+        <button id="calculate_payment" class="btn btn-lg btn-primary btn-block" type="button">Calculate</button>
+
+        <br>
+        
+        <div id="result" class="alert alert-success" role="alert">Payment: </div>
+        <div id="error" class="alert alert-danger" role="alert" style="display:none">Enter a valid Number of hours and Hourly rate</div>
+
+        <br>
+        <br>
+
+        <a href="index.php" type="button" class="btn btn-info">Back to Menu</a>
+
+      </form>
+
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/hours_worked.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+    
+  </body>
+</html>
---
+++ 3 - Algorithms/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Algorithms</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/test.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <h1 class="text-center">Algorithms</h1>
+    </div>
+
+    <div class="container">
+
+      <form class="col-sm-4 col-md-offset-4" role="form">
+       
+        <a href="number_of_blank_cds.php" class="btn btn-lg btn-primary btn-block" type="button">Number of blank CDs</a>
+        <a href="hours_worked.php" class="btn btn-lg btn-primary btn-block" type="button">Hours worked</a>
+        <a href="hangman.php" class="btn btn-lg btn-primary btn-block" type="button">Hangman</a>
+
+      </form>
+
+    </div>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+    
+  </body>
+</html>
